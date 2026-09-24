f0a128dd5b81134f252d181a2b66775e097f5f13
suraj1234-fullstack <sandhyachoudhary2005@gmail.com>
2026-09-24 16:22:36 +0530

    2

--- src/components/herosection.jsx
+++ src/components/herosection.jsx
@@ -10,7 +10,7 @@ const herosection = () => {
 
     <div className="txtbox12"style={{fontFamily:'serif',marginTop:'3%'}}>
       
-        <p style={{fontSize:'0.7rem',color:'grey',fontWeight:'700'}}><i  style={{color:"#135ac5",fontSize:'0.7rem',textAlign:'center',  boxShadow:' 0 0 0 3px #1e6bff33 ', borderRadius:'50%'}} class="fa-solid fa-circle"></i>&nbsp; &nbsp;AI-NATIVE CRICKET INTELLIENCE.</p>
+        <p  style={{fontSize:'0.7rem',color:'grey',fontWeight:'700'}}><i  style={{color:"#135ac5",fontSize:'0.7rem',textAlign:'center',  boxShadow:' 0 0 0 3px #1e6bff33 ', borderRadius:'50%'}} class="fa-solid fa-circle"></i>&nbsp; &nbsp;AI-NATIVE CRICKET INTELLIENCE</p>
 
 
        <p className='pfont124'  style={{fontSize:'5vw',fontWeight:'600'}}> <span style={{whiteSpace:"nowrap",fontFamily:'serif'}}>
